003d66de56bb575133f01d78a482e0481682875a
Lucas <lucas.batista9734@gmail.com>
2024-11-13 00:13:51 -0300

    personal_chat integracao com backend

--- routes/PersonalChatRoutes.js
+++ routes/PersonalChatRoutes.js
@@ -2,25 +2,35 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const {
-    sendPersonalMessage,
-    getPersonalMessages,
-    deletePersonalMessage,
+  sendPersonalMessage,
+  getPersonalMessages,
+  deletePersonalMessage,
 } = require("../controllers/PersonalChatController");
 const authGuard = require("../middlewares/authGuard");
-const { validateMessage, checkValidationErrors } = require("../middlewares/personalChatValidaition");
+const {
+  validateMessage,
+  checkValidationErrors,
+} = require("../middlewares/personalChatValidaition");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads/personal");
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    },
+  destination: (req, file, cb) => {
+    cb(null, "uploads/personal");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
 });
 
 const upload = multer({ storage });
 
-router.post("/personal-chat", authGuard, upload.single("image"), validateMessage, checkValidationErrors, sendPersonalMessage);
+router.post(
+  "/personal-chat",
+  authGuard,
+  upload.single("image"),
+  validateMessage,
+  checkValidationErrors,
+  sendPersonalMessage
+);
 
 router.get("/personal-chat/:userId", authGuard, getPersonalMessages);
 
