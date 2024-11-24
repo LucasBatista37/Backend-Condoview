1b580ef6a8bbc0b0357e8d76ecb934357e25472a
Lucas <lucas.batista9734@gmail.com>
2024-11-24 14:44:04 -0300

    chat imagem limite

--- routes/ChatRoutes.js
+++ routes/ChatRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const multer = require("multer");
 const router = express.Router();
 const authGuard = require("../middlewares/authGuard");
-
 const { sendMessage, getMessages, deleteMessage } = require("../controllers/ChatController");
 const validate = require("../middlewares/handleValidation");
 const { chatValidation } = require("../middlewares/chatValidation");
@@ -12,19 +11,29 @@ const storage = multer.diskStorage({
     cb(null, "uploads/chat"); 
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}_${file.originalname}`);
+    cb(null, `${Date.now()}_${file.originalname}`); 
   },
 });
+
+const upload = multer({
+  storage: storage, 
+});
+;
 
 router.post(
   "/chat",
   authGuard, 
-  chatValidation(), 
+  upload.fields([
+    { name: "image", maxCount: 1 }, 
+    { name: "file", maxCount: 1 },  
+  ]),
+  chatValidation(),
   validate, 
   sendMessage 
 );
 
-router.get("/admin/chat", authGuard, getMessages); 
-router.delete("/chat/:id", authGuard, deleteMessage); 
+router.get("/admin/chat", authGuard, getMessages);
+
+router.delete("/chat/:id", authGuard, deleteMessage);
 
 module.exports = router;
